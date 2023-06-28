ce638576586dfcbacef44ab5c3930727b9a9be22
KotaTanizawa <131939539+KotaTanizawa@users.noreply.github.com>
2023-06-29 00:00:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,27 @@ let earningsTotalAmount =  0;
 let incomeAmount = 0;
 
 const lottery = {
-  first : {tickeNumber:24,award:"1等 500,000,000円",value:500000000,color:"red"},
+  first : {tickeNumber:24,award:"1当 500,000,000円",value:500000000,color:"red"},
   frontRear : {tickeNumber:48,award:"前後賞 100,000,000円",value:100000000,color:"yellow"},
   differentSet : {tickeNumber:2376,award:"組違い賞 100,000円",value:100000,color:"orange"},
-  second : {tickeNumber:2400,award:"2等 50,000円",value:50000,color:"green"},
-  third : {tickeNumber:240000,award:"3等 10,000円",value:10000,color:"blue"},
-  four : {tickeNumber:2400000,award:"4等 3,000円",value:3000,color:"purple"},
-  five : {tickeNumber:24000000,award:"5等 300円",value:300,color:"gray"},
+  second : {tickeNumber:2400,award:"2当 50,000円",value:50000,color:"green"},
+  third : {tickeNumber:240000,award:"3当 10,000円",value:10000,color:"blue"},
+  four : {tickeNumber:2400000,award:"4当 3,000円",value:3000,color:"purple"},
+  five : {tickeNumber:24000000,award:"5当 300円",value:300,color:"gray"},
   lose : {tickeNumber:213355152,award:"はずれ",value:0,color:"black"}
 }
 
-function randum() {
-  return Math.floor(Math.random()*total);
+function randum(cheat) {
+  if (cheat === false){
+    return Math.floor(Math.random()*total);
+  }else{
+    return Math.floor(Math.random()*1);
+  }
+  
 }
 
 function drawLottery() {
-  const number = randum();
+  const number = randum(checkBox.checked);
   total--;
   let winning = 0;
   for (const key in lottery) {
@@ -33,6 +38,21 @@ function drawLottery() {
       return key;
     }
   }
+}
+
+function insertComma (value) {
+  let valueStr = String(value);
+  let result = "";
+  for (let i = 0; i <valueStr.length ; i++) {
+    if (i % 3 === 0 & i !== 0) {
+      if (valueStr[0] === "-" & valueStr.length-1 !== i | valueStr[0] !== "-"){
+        result = "," + result;
+      }
+       
+    } 
+    result = valueStr[valueStr.length - i - 1] + result
+  }
+  return result
 }
 
 function buyLottery() {
@@ -52,24 +72,19 @@ function buyLottery() {
   }
 }
 
+
+
+
 const buyButton = document.getElementById("button");
 const result = document.getElementById("result");
 const purchaseNumber = document.getElementById("purchaseNumber");
 const totalAmount = document.getElementById("totalAmount");
 const revenue = document.getElementById("revenue");
 const finalAccount = document.getElementsByClassName("finalAccount")[0];
+const checkBox = document.getElementById("checkbox");
+
+
 
 buyButton.addEventListener("click",buyLottery);
 
 
-function insertComma (value) {
-  let valueStr = String(value);
-  let result = "";
-  for (let i = 0; i <valueStr.length ; i++) {
-    if (i % 3 === 0 & i !== 0 & i !== valueStr.length) {
-      result = "," + result;
-    } 
-    result = valueStr[valueStr.length - i - 1] + result
-  }
-  return result
-}
