58b04e4399b59556e97b47e753e8ca03d7196701
KotaTanizawa <131939539+KotaTanizawa@users.noreply.github.com>
2023-07-01 14:01:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -111,9 +111,3 @@ const checkBox = document.getElementById("checkbox");
 
 
 buyButton.addEventListener("click",buyLottery);
-// buyButton.addEventListener("click", () => {
-//   button.innerHTML = `<div class="loading"></div>`;
-//   setTimeout(() => {
-//     button.innerText = "送信済み"
-//   }, 10000);
-// });
